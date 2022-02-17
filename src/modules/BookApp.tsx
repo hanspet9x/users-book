358c218acaa6792639103650e637105091736bdb
hanspet9x <pakinlolu@tawol-tech.com>
2022-02-17 16:56:41 +0100

    app init

---
+++ src/modules/BookApp.tsx
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+export default function BookApp() {
+
+  const[ready, setReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await SplashScreen.preventAutoHideAsync();
+      //load book genres here.
+    })();
+  });
+
+  if(!ready) {
+    return <></>
+  }
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
