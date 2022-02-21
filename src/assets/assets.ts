0d18521b3e88f75995dbf5acc39148ba536f0a21
hanspet9x <pakinlolu@tawol-tech.com>
2022-02-21 12:32:34 +0100

    added BookGenre container

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,3 +1,4 @@
 export const images = {
   splashScreen: require('./images/book.png'),
+  imageThumbnail: require('./images/thumbnail.png'),
 };
--- src/containers/genres/BookGenre.tsx
+++ src/containers/genres/BookGenre.tsx
@@ -1,11 +1,24 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, ImageSourcePropType, StyleSheet, Text} from 'react-native';
 import React from 'react';
+import {images} from '../../assets/assets';
+import {TouchableRipple, useTheme} from 'react-native-paper';
 
-const BookGenre = () => {
+interface Props {
+  source: ImageSourcePropType;
+  name: string;
+  url: string;
+  onGenreSelected(): void;
+}
+const BookGenre = (props: Props) => {
+  const {colors} = useTheme();
+
   return (
-    <View>
-      <Image loadingIndicatorSource={} />
-    </View>
+    <TouchableRipple rippleColor={colors.backdrop}
+      onPress={props.onGenreSelected}>
+      <Image source={props.source}
+        loadingIndicatorSource={images.imageThumbnail} />
+      <Text>{props.name}</Text>
+    </TouchableRipple>
   );
 };
 
