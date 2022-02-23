10e90fa6ef15c52175e4d655356dd31633dd8590
hanspet9x <pakinlolu@tawol-tech.com>
2022-02-23 20:10:52 +0100

    changed app name, icon

--- src/common/string.ts
+++ src/common/string.ts
@@ -2,13 +2,12 @@
 export const AppStrings = {
   genre: {
     getCheckoutURL:
-    `Please wait while we process your selection.\n
-    Kindly sign-in if required.`,
+    `Please wait while we process your selection.\nKindly sign-in if required.`,
   },
   errors: {
     notFound: 'Requested book not found! Kindly try another.!',
     timeOut: 'It seems data retrieval time is longer than expected, continue?',
-    unproccessed: 'We intend to get you one of the best from our collection. Please try again',
+    unproccessed: 'We intend to get you one of the best from our collection. Please try again.',
     badRequest: 'This request cannot be processed at this time.\nKindly contact our administrators, thanks.',
     general: 'This request cannot be processed at this time.\nKindly notify us of this experience, thanks.',
 
